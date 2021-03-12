5f936bc66af10cab296d7ab03497e188a0874ea1
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2021-03-13 01:03:24 +0300

    Snake

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,20 +1,132 @@
 // Snake.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <time.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace sf;
+
+int N = 30, M = 20;
+int size = 16;
+int w = size * N;
+int h = size * M;
+
+int dir, num = 4;
+
+struct Snake {
+	int x, y;
+} s[100];
+
+struct Fruct {
+	int x, y;
+} f;
+
+void Tick() {
+	for (int i = num; i > 0; --i) {
+		s[i].x = s[i - 1].x;
+		s[i].y = s[i - 1].y;
+	}
+
+	if (dir == 0)
+		s[0].y += 1;
+	if (dir == 1)
+		s[0].x -= 1;
+	if (dir == 2)
+		s[0].x += 1;
+	if (dir == 3)
+		s[0].y -= 1;
+
+	if ((s[0].x == f.x) && (s[0].y == f.y)) {
+		num++;
+		f.x = rand() % N;
+		f.y = rand() % M;
+	}
+
+	if (s[0].x > N)
+		s[0].x = 0;
+
+	if (s[0].x < 0)
+		s[0].x = N;
+
+	if (s[0].y > M)
+		s[0].y = 0;
+
+	if (s[0].y < 0)
+		s[0].y = M;
+
+	for (int i = 1; i < num; i++)
+		if (s[0].x == s[i].x && s[0].y == s[i].y)
+			num = i;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main(){
+	srand(time(0));
+
+	RenderWindow window(VideoMode(w, h), "Snake Game!");
+	window.setFramerateLimit(144);
+
+	Texture t1, t2;
+	t1.loadFromFile("white.png");
+	t2.loadFromFile("red.png");
+
+	Sprite sprite1(t1);
+	Sprite sprite2(t2);
+
+	Clock clock;
+	float timer = 0, delay = 0.1;
+
+	f.x = 10;
+	f.y = 10;
+
+	while (window.isOpen()) {
+		float time = clock.getElapsedTime().asSeconds();
+		clock.restart();
+		timer += time;
+
+		Event e;
+		while (window.pollEvent(e)) {
+			if (e.type == Event::Closed)
+				window.close();
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+			window.close();
+
+		if (Keyboard::isKeyPressed(Keyboard::A))
+			dir = 1;
+		if (Keyboard::isKeyPressed(Keyboard::D))
+			dir = 2;
+		if (Keyboard::isKeyPressed(Keyboard::W))
+			dir = 3;
+		if (Keyboard::isKeyPressed(Keyboard::S))
+			dir = 0;
+
+		if (timer > delay) {
+			timer = 0; 
+			Tick();
+		}
+
+		//// draw ////
+		window.clear();
+
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++) {
+				sprite1.setPosition(i * size, j * size); 
+				window.draw(sprite1);
+			}
+
+		for (int i = 0; i < num; i++) {
+			sprite2.setPosition(s[i].x * size, s[i].y * size);
+			window.draw(sprite2);
+		}
+
+		sprite2.setPosition(f.x * size, f.y * size);
+		window.draw(sprite2);
+
+		window.display();
+	}
+
+	return 0;
+}
+
